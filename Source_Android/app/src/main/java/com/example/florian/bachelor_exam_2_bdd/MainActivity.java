e45e56e3c6428740b0c622fa0ebbd6fe5a455e44
Florian Rouget <f.rouget@ludus-academie.com>
2017-03-22 17:42:07 +0100

    Commit - first commit Android Sources

---
+++ Source_Android/app/src/main/java/com/example/florian/bachelor_exam_2_bdd/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.florian.bachelor_exam_2_bdd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
